8a5d017eabdd3cd60ae795e34c7a8ad5932ab28c
VirtualS4 <Oponggunarko@yahoo.co.id>
2019-07-24 09:16:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rona/kumatraining2/Beartivity.kt
@@ -0,0 +1,12 @@
+package com.example.rona.kumatraining2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Beartivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beartivity)
+    }
+}
